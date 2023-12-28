1cd9723083847e51411257ed09d15e358ed00824
mustafacihan42 <149799256+mustafacihan42@users.noreply.github.com>
2023-12-28 14:50:31 +0300

    Update Program.cs

--- Gazi.OkulApp/Program.cs
+++ Gazi.OkulApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +16,7 @@ namespace Gazi.OkulApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmOgrKayit());
+            Application.Run(new frmOgretmenKayit());
         }
     }
 }
